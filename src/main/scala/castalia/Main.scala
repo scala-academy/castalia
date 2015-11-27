4a386a26d80c659d350d780d58b4aed84e4f51c6
Jens Kat <jens.kat@ing.nl>
2015-11-27 17:21:07 +0100

    Mix in the stubservice trait. It's now reachable at http://localhost:9000/stubs/hardcodeddummystub
    Still have to find a way to use multiple services / endpoints.

--- src/main/scala/castalia/Main.scala
+++ src/main/scala/castalia/Main.scala
@@ -4,7 +4,7 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 
-object Main extends App with Config with StatusService {
+object Main extends App with Config with StubService {
   override protected implicit val system: ActorSystem = ActorSystem()
   override protected implicit val materializer: ActorMaterializer = ActorMaterializer()
 
