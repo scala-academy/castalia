c96204f4ee0537fc8a2a1b0649ec777e4c017557
Jens <jens@jens>
2015-12-13 17:34:55 +0100

    Make CastaliaConfig use new JsonConverter.

--- src/main/scala/castalia/model/CastaliaConfig.scala
+++ src/main/scala/castalia/model/CastaliaConfig.scala
@@ -1,8 +1,9 @@
 package castalia.model
 
-import java.net.URL
-
+import castalia.JsonConverter
 import spray.json._
+
+import scala.util.Try
 
 /**
   * Created by M07H817 on 11-12-2015.
@@ -11,16 +12,10 @@ case class CastaliaConfig(httpPort: Int)
 
 object CastaliaConfig extends DefaultJsonProtocol {
   implicit val castaliaStatusResponseFormatter = jsonFormat1(CastaliaConfig.apply)
-
+  val defaultPort = 9000
   def parse(config: String): CastaliaConfig = {
-    val url = getClass.getResource("/" + config)
-    url match {
-      case url: URL =>
-        scala.io.Source.fromURL(url)
-          .mkString
-          .parseJson
-          .convertTo[CastaliaConfig]
-      case _ => CastaliaConfig(9000)
-    }
+    Try{
+      JsonConverter.parseJson[CastaliaConfig](config)
+    } getOrElse CastaliaConfig(defaultPort)
   }
 }
