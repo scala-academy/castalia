6d3b5f2761121a7ab0fd30f6277b402b6618ebf5
Jens <jens@jens>
2015-12-20 17:11:50 +0100

    Merge the separate integration tests to avoid spinning up the server twice.

--- src/it/scala/castalia/StubServerTest.scala
+++ src/it/scala/castalia/StubServerTest.scala
@@ -39,6 +39,27 @@ class StubServerTest extends IntegrationTestBase {
       assert(response.contentString == "{\n  \"id\": \"een\",\n  \"someValue\": \"123123\"\n}")
 
     }
+
+    it("Should allow the user can configure endpoint name in json file") {
+
+      info("background:")
+      info("given that the stubserver is started and configured to use a stub called \"jsonconfiguredstub\"")
+      info("and that a stub config file with name \"jsonconfiguredstub.json\" is on the classpath")
+      //[Note: for now i use hardcoded StubConfig object in Routes]
+
+      Given("jsonconfiguredstub.json contains a property \"endpoint\" equal to \"jsonconfiguredstub\"")
+
+      val client = finagle.Http.newService(serverAddress)
+      //TODO: make here a jsonconfiguredstub.json file and put it to classpath
+
+      When("I do a HTTP GET to .../stubs/jsonconfiguredstub")
+      val request = Request(Method.Get, "/status")
+      request.host = serverAddress
+      val response = Await.result(client(request))
+
+      Then("then I should get a HTTP 200 response")
+      assert(response.statusCode == 200)
+    }
   }
 
 
