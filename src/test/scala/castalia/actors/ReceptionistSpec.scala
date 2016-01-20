5f41704f48e62d0f85d613847310f232f034c71b
rezolya <rezolya@yandex.ru>
2016-01-20 08:09:31 +0100

    start reuse StubServiceSpec for receptionist

--- src/test/scala/castalia/actors/ReceptionistSpec.scala
+++ src/test/scala/castalia/actors/ReceptionistSpec.scala
@@ -1,34 +1,61 @@
-//package castalia
+package castalia.actors
+
 //
 //import akka.actor.ActorSystem
-//import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-//import akka.http.scaladsl.model.ContentTypes.`application/json`
-//import akka.http.scaladsl.model.StatusCodes._
-//import akka.testkit.EventFilter
-//import castalia.StubConfigParser._
-//import castalia.model.CastaliaConfig._
-//import castalia.model.ResponseConfig
-//import com.typesafe.config.ConfigFactory
-//import spray.json._
-//
-///**
-//  * Created by Jens Kat on 25-11-2015.
-//  */
-//// TODO reuse these tests for Receptionist
-//class StubServiceSpec extends ServiceTestBase with SprayJsonSupport {
-//
-//  val responseByEndpoint = parseStubConfigs(List("jsonconfiguredstub.json"))
-//  val service = new StubService(responseByEndpoint)
-//
-//  "A request to a non-existing endpoint" should {
-//    "result in HTTP status code 404" in {
-//      Get("/stubs/nonexistingstub") ~> service.routes ~> check {
-//        status shouldBe NotFound
-//        responseAs[String] shouldBe "Not Found"
-//      }
-//    }
-//  }
-//
+import akka.actor.ActorSystem
+import akka.http.impl.server.RequestContextImpl
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.model.ContentTypes.`application/json`
+import akka.http.scaladsl.model.{HttpMethods, HttpRequest}
+import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.server.RequestContext
+import akka.testkit.EventFilter
+import castalia.StubConfigParser._
+import castalia._
+import castalia.model.CastaliaConfig._
+import castalia.model.Messages.{Done, UpsertEndpoint}
+import castalia.model.Model.StubResponse
+import com.typesafe.config.ConfigFactory
+
+class ReceptionistSpec(_system: ActorSystem) extends ActorSpecBase(_system)  {
+
+  def this() = this(ActorSystem("ReceptionistSpec"))
+
+  val receptionist = system.actorOf(Receptionist.props)
+
+  val stubConfig = parseStubConfigs(List("jsonconfiguredstub.json")).head
+
+  "Receptionist actor" when {
+    "receives UpsertEndpoint message" should {
+      "process it and reply with Done" in {
+        receptionist ! UpsertEndpoint(stubConfig)
+        expectMsg(Done(stubConfig.endpoint))
+      }
+    }
+
+    "receives a request to a non-existing endpoint" should {
+      "return HTTP status code 404" in {
+        //val r = HttpRequest(HttpMethods.GET, "/stubs/nonexistingstub" )
+        //receptionist ! RequestContext(
+        //  request = r,
+        //  responder = receptionist,
+        //  unmatchedPath = request.path
+        //)
+        //TODO send RequestContext to "/stubs/nonexistingstub")
+        expectMsg(StubResponse(404, "From receptionist " + NotFound.reason))
+      }
+    }
+
+    "receives a request to an existing endpoint " should {
+      "forward the request to the endpoint" in {
+        ???
+      }
+    }
+
+  }
+
+
+
 //  "A HTTP GET request to '/stubs/doublepathparam/0/responsedata/notfound' " should {
 //    "result in a HTTP 404 response from the stubserver" in {
 //      Get(s"/stubs/doublepathparam/0/responsedata/notfound") ~> service.routes ~> check {
@@ -93,4 +120,4 @@
 //      }
 //    }
 //  }
-//}
+}
