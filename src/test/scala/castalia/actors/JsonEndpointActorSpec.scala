2779dd3f7333057f890e1d05d004017b1e39b303
Jens <jens@jens>
2016-01-21 08:11:03 +0100

    Replace currentTimeMillis with 'within'.
    Too strict timing in unittest caused TravisCI to fail the test (even on my machine). Hopefully a looser timing (1200.millis) will cause the test to succeed.

--- src/test/scala/castalia/actors/JsonEndpointActorSpec.scala
+++ src/test/scala/castalia/actors/JsonEndpointActorSpec.scala
@@ -5,6 +5,7 @@ import akka.http.scaladsl.model.Uri
 import castalia.StubConfigParser._
 import castalia.matcher.RequestMatch
 import castalia.model.Model.StubResponse
+import scala.concurrent.duration._
 
 class JsonEndpointActorSpec(_system: ActorSystem) extends ActorSpecBase(_system) {
 
@@ -13,32 +14,28 @@ class JsonEndpointActorSpec(_system: ActorSystem) extends ActorSpecBase(_system)
   "JsonEndpointActorTest" should {
 
     "receive" in {
-      val uriString = "somepath/2"
-      val uri: Uri = uriString
-      val jsonConfig = parseStubConfig("jsonsimplestub.json")
-      val jsonEndpoint = system.actorOf(Props(new JsonEndpointActor(jsonConfig)))
-      //RequestMatch(uri: String, path: Path, pathParams: Params, queryParams: Params, handler: String)
+        val uriString = "somepath/2"
+        val uri: Uri = uriString
+        val jsonConfig = parseStubConfig("jsonsimplestub.json")
+        val jsonEndpoint = system.actorOf(Props(new JsonEndpointActor(jsonConfig)))
+        //RequestMatch(uri: String, path: Path, pathParams: Params, queryParams: Params, handler: String)
 
-      val t1 = System.currentTimeMillis()
-      jsonEndpoint ! new RequestMatch( uriString, uri.path, List(("$1", "1")), Nil, jsonEndpoint )
+      within(1000.millis, 1200.millis) {
+        jsonEndpoint ! new RequestMatch(uriString, uri.path, List(("$1", "1")), Nil, jsonEndpoint)
 
-      expectMsg(StubResponse(200, """{"id":"een","someValue":"123123"}"""))
-      assert(System.currentTimeMillis() > 1000 + t1)
-      assert(System.currentTimeMillis() < 1100 + t1)
+        expectMsg(StubResponse(200, """{"id":"een","someValue":"123123"}"""))
+      }
 
-      val t2 = System.currentTimeMillis()
-      jsonEndpoint ! new RequestMatch( uriString, uri.path, List(("$1", "2")), Nil, jsonEndpoint )
+      within(100.millis, 200.millis) {
+        jsonEndpoint ! new RequestMatch(uriString, uri.path, List(("$1", "2")), Nil, jsonEndpoint)
 
-      expectMsg(StubResponse(200, """{"id":"twee","someValue":"2222"}"""))
-      assert(System.currentTimeMillis() > 100 + t2)
-      assert(System.currentTimeMillis() < 200 + t2)
+        expectMsg(StubResponse(200, """{"id":"twee","someValue":"2222"}"""))
+      }
+      within(50.millis) {
+        jsonEndpoint ! new RequestMatch(uriString, uri.path, List(("$1", "3")), Nil, jsonEndpoint)
 
-      val t3 = System.currentTimeMillis()
-      jsonEndpoint ! new RequestMatch( uriString, uri.path, List(("$1", "3")), Nil, jsonEndpoint )
-
-      expectMsg(StubResponse(200, """{"id":"drie","someValue":"123123"}"""))
-      assert(System.currentTimeMillis() < 50 + t3)
-
+        expectMsg(StubResponse(200, """{"id":"drie","someValue":"123123"}"""))
+      }
     }
 
   }
