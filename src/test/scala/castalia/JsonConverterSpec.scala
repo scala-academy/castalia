e898f8dc05bf2a1f94f0851adee803167bf65348
Jean-Marc van Leerdam <jmvanleerdam@gmail.com>
2016-01-21 18:02:02 +0100

    Wire in default repsonse configuration

--- src/test/scala/castalia/JsonConverterSpec.scala
+++ src/test/scala/castalia/JsonConverterSpec.scala
@@ -2,14 +2,14 @@ package castalia
 
 import java.io.FileNotFoundException
 
-import castalia.model.Model.{LatencyConfig, ResponseConfig, StubConfig}
+import castalia.model.Model.{DefaultResponseConfig, LatencyConfig, ResponseConfig, StubConfig}
 import org.scalatest.{Matchers, WordSpec}
 
 class JsonConverterSpec extends UnitSpecBase {
 
-  "A JsonConverter object" when {
+  "The Json Converter objects" when {
 
-    "parsing a existing json file to a matching type" should {
+    "parsing a existing json stub config file to a matching type" should {
       "return correctly parsed object" in {
         val stubconfig = JsonConverter.parseJson[StubConfig]("jsonconfiguredstub.json")
         stubconfig.endpoint.shouldBe("doublepathparam/$1/responsedata/$2")
@@ -29,11 +29,20 @@ class JsonConverterSpec extends UnitSpecBase {
     }
 
     "parsing an existing json file to a wrong type" should {
-        "throw UnmarshalException" in {
-          intercept[UnmarshalException] {
-            JsonConverter.parseJson[ResponseConfig]("jsonconfiguredstub.json")
-          }
+      "throw UnmarshalException" in {
+        intercept[UnmarshalException] {
+          JsonConverter.parseJson[ResponseConfig]("jsonconfiguredstub.json")
         }
       }
     }
+
+    "parsing a stubconfig with default" should {
+      "return correctly parsed stubconfig" in {
+        val stubConfig = JsonConverter.parseJson[StubConfig]("sharedproperties.json")
+
+        stubConfig.endpoint.shouldBe("somestub/$parm")
+        stubConfig.default.shouldBe(Some(DefaultResponseConfig(Some(LatencyConfig("constant", "100 ms")), None, None)))
+      }
+    }
+  }
 }
