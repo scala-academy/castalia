c81bd946a5c6ecf00a410fc88662b4a05f37712d
Jordi de Vos <devos.jordi@gmail.com>
2016-02-07 18:53:10 +0100

    Refactored tuple to map for better readability

--- src/main/scala/castalia/matcher/RequestMatcherActor.scala
+++ src/main/scala/castalia/matcher/RequestMatcherActor.scala
@@ -22,16 +22,16 @@ object RequestMatcherActor {
 class RequestMatcherActor extends Actor with ActorLogging {
   this: RequestMatcherActorCreator =>
 
-  def receive: Receive = normal(Set.empty[(ActorRef, Segments)])
+  def receive: Receive = normal(Map.empty[Segments, ActorRef])
 
-  def normal(matchers: Set[(ActorRef, Segments)]): Receive = {
+  def normal(matchers: Map[Segments, ActorRef]): Receive = {
     // Add matcher. If there is already a matcher with the same segments, stop that and replace with new one
     case AddMatcher(segments, handler) =>
       val matcher = createRequestMatcherActor(context, segments, handler)
       log.debug(s"Added matcher $matcher to RequestMatcherActor ${self.toString()}")
-      val matcherOnSameSegments = matchers.find(_._2.equals(segments))
-      matcherOnSameSegments.foreach { case (matcherRef, _) => context.stop(matcherRef) }
-      val newSetOfMatchers = matchers.filter(!_._2.equals(segments)) + ((matcher, segments))
+      val matcherOnSameSegments = matchers.get(segments)
+      matcherOnSameSegments.foreach { matcherRef => context.stop(matcherRef) }
+      val newSetOfMatchers = matchers.updated(segments, matcher)
       context.become(normal(newSetOfMatchers))
 
     // Foward match request to all registered matchers. Create result gatherer to gather and handle results
@@ -39,11 +39,10 @@ class RequestMatcherActor extends Actor with ActorLogging {
       val parsedUri = new ParsedUri(httpRequest.uri.toString().replace(';', '&'), httpRequest.uri.path, httpRequest.uri.query().toList)
       log.debug(s"RequestMatcherActor received http request $parsedUri from $sender")
       val gatherer = context.actorOf(MatchResultGatherer.props(matchers.size, origin))
-      matchers.foreach(matcher => {
-        log.debug(s"sending to ${matcher._1} ${RespondIfMatched(parsedUri, httpRequest, gatherer)}")
-        val matcherRef = matcher._1
-        matcherRef ! RespondIfMatched(parsedUri, httpRequest, gatherer)
-      })
+      matchers.foreach{case (segments, matcher) => {
+        log.debug(s"sending to ${matcher} ${RespondIfMatched(parsedUri, httpRequest, gatherer)}")
+        matcher ! RespondIfMatched(parsedUri, httpRequest, gatherer)
+      }}
 
     // unexpected messages
     case x =>
